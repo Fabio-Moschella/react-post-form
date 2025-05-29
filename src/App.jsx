9fcce0685e2abd134f63bfa3b89a8fb6026b928f
Fabio-Moschella <fabiomoschella1999@gmail.com>
2025-05-29 15:57:19 +0200

    add-form-starter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
 import { useState } from "react";
 
-function App() {}
+function App() {
+  return (
+    <form className="form'control">
+      <label className="form'label"></label>
+      <input type="text" className="form'control" />
+    </form>
+  );
+}
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import * as bootstrap from "bootstrap";
+
 import "./assets/css/index.css";
+
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
