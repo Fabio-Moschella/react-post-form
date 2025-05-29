e762d3cfc3a3ae0b0642d43a823ecacff6aa68b2
Fabio-Moschella <fabiomoschella1999@gmail.com>
2025-05-29 17:44:23 +0200

    call post api send

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,35 @@
 import { useState } from "react";
+import axios from "axios";
 const apiUrl = "https://67c5b4f3351c081993fb1ab6.mockapi.io/api/posts";
 function App() {
+  const handleForSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post(apiUrl, {
+        author: "fabio",
+        title: "thungsaur",
+        body: "bastone",
+        public: true,
+      })
+      .then((res) => {
+        const response = res.data;
+        console.log(response);
+      });
+  };
+
   return (
-    <form className="form-control" onSubmit={""}>
+    <form className="form-control" onSubmit={handleForSubmit}>
       <label className="form-label">AUTHOR</label>
-      <input type="text" className="form-control" />
+      <input type="text" name="author" className="form-control" />
       <label className="form-label">TITLE</label>
-      <input type="text" className="form-control" />
+      <input type="text" name="title " className="form-control" />
       <label className="form-label">BODY</label>
-      <input type="text" className="form-control" />
+      <input type="text" name="body" className="form-control" />
       <div className="mb-3 form-check">
         <input type="checkbox" className="form-check-input" />
-        <label className="form-check-label">Check me out</label>
+        <label className="form-check-label"></label>
       </div>
+      <button type="submit">Invia</button>
     </form>
   );
 }
