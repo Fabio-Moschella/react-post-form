209ad18e987fc145dcd17aa118705d356d7f7e1c
Fabio-Moschella <fabiomoschella1999@gmail.com>
2025-05-29 19:49:36 +0200

    add-formdata on the html

--- src/App.jsx
+++ src/App.jsx
@@ -8,30 +8,67 @@ function App() {
     body: "bastone",
     public: false,
   });
+  const handleChange = (e) => {
+    const { name, value, type, checked } = e.target;
+    setFormData((data) => ({
+      ...data,
+      [name]: type === "checkbox" ? checked : value,
+    }));
+  };
+
   const handleForSubmit = (e) => {
     e.preventDefault();
+
     axios
-      .post(apiUrl, {
-        formData,
-      })
+      .post(apiUrl, formData)
       .then((res) => {
-        const response = res.data;
-        console.log(response);
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.error("Errore nella POST:", err);
       });
   };
 
   return (
     <form className="form-control" onSubmit={handleForSubmit}>
       <label className="form-label">AUTHOR</label>
-      <input type="text" name="author" className="form-control" />
+      <input
+        type="text"
+        name="author"
+        value={formData.author}
+        onChange={handleChange}
+        className="form-control"
+      />
+
       <label className="form-label">TITLE</label>
-      <input type="text" name="title " className="form-control" />
+      <input
+        type="text"
+        name="title"
+        value={formData.title}
+        onChange={handleChange}
+        className="form-control"
+      />
+
       <label className="form-label">BODY</label>
-      <input type="text" name="body" className="form-control" />
+      <input
+        type="text"
+        name="body"
+        value={formData.body}
+        onChange={handleChange}
+        className="form-control"
+      />
+
       <div className="mb-3 form-check">
-        <input type="checkbox" className="form-check-input" />
-        <label className="form-check-label"></label>
+        <input
+          type="checkbox"
+          name="public"
+          checked={formData.public}
+          onChange={handleChange}
+          className="form-check-input"
+        />
+        <label className="form-check-label">Public</label>
       </div>
+
       <button className="btn btn-primary" type="submit">
         Invia
       </button>
